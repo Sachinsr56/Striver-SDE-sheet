84d6d42ecedf0cefb47b19bcca94ed4b4f02235a
Sachinsr56 <99339565+Sachinsr56@users.noreply.github.com>
2022-08-13 07:08:40 +0530

    Rabin Karp

---
+++ String/Rabin Karp.cpp
@@ -0,0 +1,65 @@
+
+//  --------------suing str str--------
+vector<int> stringMatch(string &str, string &pat) {
+    int n=str.length();
+    int m=pat.length();
+    vector<int> v;
+    for(int i=0; i<n; i++)
+    {
+        if(str[i]==pat[0])
+        {
+            if(str.substr(i, m)==pat)
+            {
+                v.push_back(i);
+            }
+        }
+    }
+    return v;
+}
+
+// -----using rabin karp-----------------
+
+#include<bits/stdc++.h>
+const int base=31;
+const int q=29;
+
+void rabinkarp(string &pattern,string &text,vector<int> &ans){
+    int j;
+    int h=1;
+    int p=0;
+    int t=0;
+    int n=text.size();
+    int m=pattern.size();
+    
+    for(int i=0; i<m-1; i++)
+        h=(h*base)%q;
+    
+    for(int i=0; i<m; i++){
+        p=(base*p+pattern[i])%q;
+        t=(base*t+text[i])%q;
+    }
+    for(int i=0; i<=n-m; i++){
+        if(p==t){
+            for(j=0; j<m; j++){
+                if(text[i+j]!=pattern[j]){
+                    break;
+                }
+            }
+            if(j==m) ans.push_back(i);
+        }
+        if(i<n-m){
+            t=(t-h*text[i]);
+            t=(base*t+text[i+m])%q;
+            if(t<0) 
+                t=t+q;
+        }
+    }
+}
+
+vector<int> stringMatch(string &str, string &pat) {
+    // Write your code here
+    vector<int> ans;
+    rabinkarp(pat,str,ans);
+    return ans;
+}
+
